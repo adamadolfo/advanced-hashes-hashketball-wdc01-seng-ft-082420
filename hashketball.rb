59bf15dca828528f918e00f990781743104aebd5
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-13 21:11:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,7 +166,7 @@ end
         
 def player_stats(player_name)
   game_hash.each do |home_away, team_data|
-    team_data[:players].each do |x, y|
+    team_data[:players].each do |player|
       binding.pry
     end
   end
